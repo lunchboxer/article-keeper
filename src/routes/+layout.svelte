d634191e658050061316b3464e3bee96a617c1f9
James Smith <mrsmith@aliyun.com>
2023-07-19 20:46:58 +0800

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<title>Article-a-day</title>
+</svelte:head>
+<div class="container">
+	<slot />
+</div>
+
+<style>
+	.container {
+		margin: 0 auto;
+		max-width: 960px;
+		padding: 2rem;
+	}
+</style>
